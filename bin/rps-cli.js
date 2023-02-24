9d47cdc54b3b81aaf1b0c391f79da92c9af95350
xiaohany <rgnyu99@gmail.com>
2023-02-24 15:52:05 -0400

    update

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,22 +3,6 @@ import { rps } from "../lib/rpsls.js";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-const hlp = `Usage: node-rps [SHOT]
-Play Rock Paper Scissors (RPS)
-
-  -h, --help      display this help message and exit
-  -r, --rules     display the rules and exit
-
-Examples:
-  node-rps        Return JSON with single player RPS result.
-                  e.g. {"player":"rock"}
-  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}`;
-const rl = `Rules for Rock Paper Scissors:
-
-  - Scissors CUTS Paper
-  - Paper COVERS Rock
-  - Rock CRUSHES Scissors`;
 
 if (args.h || args.help) {
   printhlp();
@@ -31,10 +15,24 @@ if (args.r || args.rule) {
 }
 
 function printhlp() {
-  console.log(hlp);
+  console.log(`Usage: node-rps [SHOT]
+  Play Rock Paper Scissors (RPS)
+  
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+  
+  Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`);
 }
 function printrl() {
-  console.log(rl);
+  console.log(`Rules for Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
 }
 
 var len = args._length;
@@ -55,7 +53,7 @@ switch (len) {
       process.exit(0);
     }
   default:
-    console.log("Out of range");
+    console.log("[ARGUMENT] is out of range.");
     printhlp();
     printrl();
     process.exit(0);
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -4,31 +4,6 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 
-const hlp = `Usage: node-rpsls [SHOT]
-Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
-  -h, --help        display this help message and exit
-  -r, --rules       display the rules and exit
-
-Examples:
-  node-rpsls        Return JSON with single player RPSLS result.
-                    e.g. {"player":"rock"}
-  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
-
-const rl = `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
-
-- Scissors CUTS Paper
-- Paper COVERS Rock
-- Rock SMOOSHES Lizard
-- Lizard POISONS Spock
-- Spock SMASHES Scissors
-- Scissors DECAPITATES Lizard
-- Lizard EATS Paper
-- Paper DISPROVES Spock
-- Spock VAPORIZES Rock
-- Rock CRUSHES Scissors`;
-
 if (args.h || args.help) {
   printhlp();
   process.exit(0);
@@ -40,10 +15,31 @@ if (args.r || args.rule) {
 }
 
 function printhlp() {
-  console.log(hlp);
+  console.log(`Usage: node-rpsls [SHOT]
+  Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+  
+    -h, --help        display this help message and exit
+    -r, --rules       display the rules and exit
+  
+  Examples:
+    node-rpsls        Return JSON with single player RPSLS result.
+                      e.g. {"player":"rock"}
+    node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
 }
 function printrl() {
-  console.log(rl);
+  console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock SMOOSHES Lizard
+  - Lizard POISONS Spock
+  - Spock SMASHES Scissors
+  - Scissors DECAPITATES Lizard
+  - Lizard EATS Paper
+  - Paper DISPROVES Spock
+  - Spock VAPORIZES Rock
+  - Rock CRUSHES Scissors`);
 }
 
 var len = args._length;
@@ -70,7 +66,7 @@ switch (len) {
       process.exit(0);
     }
   default:
-    console.log("Out of range");
+    console.log("[ARGUMENT] is out of range.");
     printhlp();
     printrl();
     process.exit(0);
