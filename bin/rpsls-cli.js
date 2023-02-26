5dd83ff8da46fac867793adfac251eaced04d684
xiaohany <rgnyu99@gmail.com>
2023-02-26 16:43:04 -0400

    sixth

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -2,118 +2,19 @@
 import { rpsls } from "../lib/rpsls.js";
 import minimist from "minimist";
 
-// const args = minimist(process.argv.slice(2));
-
-// if (args.h || args.help) {
-//   printhlp();
-//   process.exit(0);
-// }
-
-// if (args.r || args.rules) {
-//   printrl();
-//   process.exit(0);
-// }
-
-// function printhlp() {
-//   console.log(`Usage: node-rpsls [SHOT]
-//   Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
-//     -h, --help        display this help message and exit
-//     -r, --rules       display the rules and exit
-
-//   Examples:
-//     node-rpsls        Return JSON with single player RPSLS result.
-//                       e.g. {"player":"rock"}
-//     node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-//                       e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
-// }
-// function printrl() {
-//   console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
-
-//   - Scissors CUTS Paper
-//   - Paper COVERS Rock
-//   - Rock SMOOSHES Lizard
-//   - Lizard POISONS Spock
-//   - Spock SMASHES Scissors
-//   - Scissors DECAPITATES Lizard
-//   - Lizard EATS Paper
-//   - Paper DISPROVES Spock
-//   - Spock VAPORIZES Rock
-//   - Rock CRUSHES Scissors`);
-// }
-
-// var len = args._length;
-
-// switch (len) {
-//   case len == 0:
-//     console.log(JSON.stringify(rpsls("none")));
-//     process.exit(0);
-//   case len == 1:
-//     var player = args._[0].toString().toLowerCase();
-//     if (
-//       player == "rock" ||
-//       player == "paper" ||
-//       player == "scissors" ||
-//       player == "lizard" ||
-//       player == "spock"
-//     ) {
-//       console.log(JSON.stringify(rpsls(player)));
-//       process.exit(0);
-//     } else {
-//       console.log("Invalid input");
-//       printhlp();
-//       printrl();
-//       process.exit(0);
-//     }
-//   default:
-//     console.log("[ARGUMENT] is out of range.");
-//     printhlp();
-//     printrl();
-//     process.exit(0);
-// }
-
 const args = minimist(process.argv.slice(2));
 
-if (args.help || args.h) {
-  printHelp();
+if (args.h || args.help) {
+  printhlp();
   process.exit(0);
 }
 
-if (args.rules || args.r) {
-  printRules();
+if (args.r || args.rules) {
+  printrl();
   process.exit(0);
 }
-var locz = args._length;
 
-switch (locz) {
-  case locz == 0:
-    console.log(JSON.stringify(rpsls("none")));
-    process.exit(0);
-  case locz == 1:
-    var player = args._[0].toString().toLowerCase();
-    if (
-      player == "rock" ||
-      player == "paper" ||
-      player == "scissors" ||
-      player == "lizard" ||
-      player == "spock"
-    ) {
-      console.log(JSON.stringify(rpsls(player)));
-      process.exit(0);
-    } else {
-      console.log("unknown input");
-      printHelp();
-      printRules();
-      process.exit(0);
-    }
-  default:
-    console.log(" out of range.");
-    printHelp();
-    printRules();
-    process.exit(0);
-}
-
-function printHelp() {
+function printhlp() {
   console.log(`Usage: node-rpsls [SHOT]
     Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
     
@@ -126,8 +27,7 @@ function printHelp() {
       node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
                         e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
 }
-
-function printRules() {
+function printrl() {
   console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
     - Scissors CUTS Paper
     - Paper COVERS Rock
@@ -139,4 +39,34 @@ function printRules() {
     - Paper DISPROVES Spock
     - Spock VAPORIZES Rock
     - Rock CRUSHES Scissors`);
+}
+
+var len = args._length;
+
+switch (len) {
+  case len == 0:
+    console.log(JSON.stringify(rpsls("none")));
+    process.exit(0);
+  case len == 1:
+    var player = args._[0].toString().toLowerCase();
+    if (
+      player == "rock" ||
+      player == "paper" ||
+      player == "scissors" ||
+      player == "lizard" ||
+      player == "spock"
+    ) {
+      console.log(JSON.stringify(rpsls(player)));
+      process.exit(0);
+    } else {
+      console.log("Invalid input");
+      printhlp();
+      printrl();
+      process.exit(0);
+    }
+  default:
+    console.log("[ARGUMENT] is out of range.");
+    printhlp();
+    printrl();
+    process.exit(0);
 }
